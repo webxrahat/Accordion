69369fef3cd6d4b52013f148d45133e9fa8b8558
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-09 07:50:51 +0600

    array of object

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 import { MdSwipeDown } from "react-icons/md";
 
+const quizs = [
+ {
+  question: "What is the capital of France?",
+  answer: "Paris",
+ },
+ {
+  question: "Who wrote the play 'Romeo and Juliet'?",
+  answer: "William Shakespeare",
+ },
+ {
+  question: "What is the smallest planet in our solar system?",
+  answer: "Mercury",
+ },
+ {
+  question: "What is the process by which plants make their own food?",
+  answer: "Photosynthesis",
+ },
+ {
+  question: "Who is known as the father of modern physics?",
+  answer: "Albert Einstein",
+ },
+ {
+  question: "What is the chemical symbol for water?",
+  answer: "H2O",
+ },
+];
+
 const Accordion = () => {
  const [isHover, setIsHover] = useState(false);
 
@@ -8,22 +35,24 @@ const Accordion = () => {
   setIsHover(true);
  };
 
- console.log(isHover);
+ //  console.log(isHover);
 
  return (
   <div>
-   <div className="max-w-3xl mx-auto">
-    <div
-     onMouseEnter={handleMouseHover}
-     className="flex w-full justify-between flex-row-reverse bg-gray-100"
-    >
-     <MdSwipeDown size={30} color="green" />
-     <h2 className="text-xl">Question</h2>
+   {quizs.map((qus, index) => (
+    <div key={index} className="max-w-3xl mx-auto">
+     <div
+      onMouseEnter={handleMouseHover}
+      className="flex w-full justify-between flex-row-reverse bg-gray-100"
+     >
+      <MdSwipeDown size={30} color="green" />
+      <h2 className="text-xl">{qus.question}</h2>
+     </div>
+     <div className={`${isHover ? "visible" : "invisible"}`}>
+      <p>Answer</p>
+     </div>
     </div>
-    <div className={`${isHover ? "visible" : "invisible"}`}>
-     <p>Answer</p>
-    </div>
-   </div>
+   ))}
   </div>
  );
 };
